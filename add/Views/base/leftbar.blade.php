df686414ffa5ebdd1347cb90e2826e6b60d045df
babon7x <babon7x@gmail.com>
2022-07-23 16:44:19 +0700

    first commit

--- add/Views/base/leftbar.blade.php
+++ add/Views/base/leftbar.blade.php
@@ -1,93 +1,46 @@
-<div class="mainnav__categoriy py-3 ">
-    <ul class="mainnav__menu nav flex-column ">
-        <li class="side-head">
-            <h6 class="mainnav__caption mt-0 px-3 fw-bold  text-primary">Home</h6>
-        </li>
-        <li class="nav-item side-list">
-            <a href="/home" url="/home" class="nav-link mininav-toggle ">
-                <i class="ti-home  fs-5 me-2"></i>
-                <span class="nav-label mininav-content ms-1">Dashboard</span>
-            </a>
-        </li>
-        <li class="side-space">
-            <hr class="">
-        </li>
-        <li class="side-head">
-            <h6 class="mainnav__caption mt-0 px-3 fw-bold  text-primary">Master
-            </h6>
-        </li>
-        <li class="nav-item has-sub side-child">
-            <a href="#" class="mininav-toggle nav-link collapsed  ">
-                <i class="ti-crown fs-5 me-2"></i>
-                <span class="nav-label ms-1">Akses</span>
-            </a>
-
-            <ul class="mininav-content collapse nav ">
-                <li class="nav-item side-list">
-                    <a href="/role" url="/role" class="nav-link mininav-toggle ">
-                        <i class="ti-link fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">Role</span>
-                    </a>
-                </li>
-                <li class="nav-item side-list">
-                    <a href="/user" url="/user" class="nav-link mininav-toggle ">
-                        <i class="ti-link fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">User</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="nav-item has-sub side-child">
-            <a href="#" class="mininav-toggle nav-link collapsed  ">
-                <i class="ti-link fs-5 me-2"></i>
-                <span class="nav-label ms-1">Operasional</span>
-            </a>
-
-            <ul class="mininav-content collapse nav ">
-                <li class="nav-item side-list">
-                    <a href="/aplikasi" url="/aplikasi" class="nav-link mininav-toggle ">
-                        <i class="ti-home  fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">aplikasi</span>
-                    </a>
-                </li>
-                <li class="nav-item side-list">
-                    <a href="/kode_error" url="/kode_error" class="nav-link mininav-toggle ">
-                        <i class="ti-home  fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">kode error</span>
-                    </a>
-                </li>
-                <li class="nav-item side-list">
-                    <a href="/layanan" url="/layanan" class="nav-link mininav-toggle ">
-                        <i class="ti-home  fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">layanan</span>
-                    </a>
-                </li>
-                <li class="nav-item side-list">
-                    <a href="/tingkat_prioritas" url="/tingkat_prioritas" class="nav-link mininav-toggle ">
-                        <i class="ti-home  fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">tingkat prioritas</span>
-                    </a>
-                </li>
-                <li class="nav-item side-list">
-                    <a href="/kategori_perbaikan" url="/kategori_perbaikan" class="nav-link mininav-toggle ">
-                        <i class="ti-home  fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">kategori perbaikan</span>
-                    </a>
-                </li>
-                <li class="nav-item side-list">
-                    <a href="/perbaikan" url="/perbaikan" class="nav-link mininav-toggle ">
-                        <i class="ti-home  fs-5 me-2"></i>
-                        <span class="nav-label mininav-content ms-1">perbaikan</span>
-                    </a>
-                </li>
-                
-            </ul>
-        </li>
-        <li class="side-space">
-            <hr class="">
-        </li>
-        <li class="side-head">
-            <h6 class="mainnav__caption mt-0 px-3 fw-bold  text-primary">Laporan</h6>
-        </li>
-    </ul>
-</div>
+<div class="mainnav__categoriy py-3 "><ul class="mainnav__menu nav flex-column "><li class="nav-item side-list">
+                        <a href="/home" url="/home" class="nav-link mininav-toggle ">
+                            <i class="ti-home  fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">Dashboard</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/role" url="/role" class="nav-link mininav-toggle ">
+                            <i class="ti-link fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">Role</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/user" url="/user" class="nav-link mininav-toggle ">
+                            <i class="ti-link fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">User</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/aplikasi" url="/aplikasi" class="nav-link mininav-toggle ">
+                            <i class="ti-home  fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">aplikasi</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/kode_error" url="/kode_error" class="nav-link mininav-toggle ">
+                            <i class="ti-home  fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">kode error</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/layanan" url="/layanan" class="nav-link mininav-toggle ">
+                            <i class="ti-home  fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">layanan</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/tingkat_prioritas" url="/tingkat_prioritas" class="nav-link mininav-toggle ">
+                            <i class="ti-home  fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">tingkat prioritas</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/kategori_perbaikan" url="/kategori_perbaikan" class="nav-link mininav-toggle ">
+                            <i class="ti-home  fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">kategori perbaikan</span>
+                        </a>
+                    </li><li class="nav-item side-list">
+                        <a href="/perbaikan" url="/perbaikan" class="nav-link mininav-toggle ">
+                            <i class="ti-home  fs-5 me-2"></i>
+                            <span class="nav-label mininav-content ms-1">perbaikan</span>
+                        </a>
+                    </li></ul></div>
