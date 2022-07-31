e46405970587f8b411132a019ddef6f16c084a1b
babon7x <babon7x@gmail.com>
2022-07-31 11:40:43 +0700

    fix bug

--- add/Controllers/LoginController.php
+++ add/Controllers/LoginController.php
@@ -66,7 +66,7 @@ class LoginController extends Controller
 		// $store = User::create($data);
 
 		$store = User::create([
-			'role_id' =>3,
+			'role_id' =>4,
 			'username' => $request['username'],
 			'email' => $request['email'],
 			'password' => Hash::make($request['password']),
